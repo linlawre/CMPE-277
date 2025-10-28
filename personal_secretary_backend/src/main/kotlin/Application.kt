34fb57f15a5ee720584aed1e02f80725c02d6fd0
Tim Nguyen <timmyn97@gmail.com>
2025-10-27 21:55:26 -0700

    Initial backend set up. Edited backend to work with MongoDB initial connection to one DB

---
+++ personal_secretary_backend/src/main/kotlin/Application.kt
@@ -0,0 +1,56 @@
+package com.example.personal_secretary_backend
+
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.routing.*
+import io.ktor.server.response.*
+import kotlinx.coroutines.runBlocking
+import org.litote.kmongo.coroutine.coroutine
+import org.litote.kmongo.coroutine.CoroutineCollection
+import org.litote.kmongo.reactivestreams.KMongo
+import java.util.Properties
+import java.io.FileInputStream
+
+@kotlinx.serialization.Serializable
+data class Notes(
+    val id: Int,
+    val date: String,
+    val location: String? = null,
+    val description: String
+)
+
+val props = Properties().apply {
+    FileInputStream("src/main/resources/config.properties").use { load(it) }
+}
+val mongoUri = props.getProperty("mongodb_uri")
+val client = KMongo.createClient(mongoUri).coroutine
+val database = client.getDatabase("notesdb")
+val notesCollection: CoroutineCollection<Notes> = database.getCollection()
+fun Application.module() {
+    install(ContentNegotiation) {
+        json()
+    }
+
+    runBlocking {
+        println("=== Notes in database ===")
+        notesCollection.find().toList().forEach { println(it) }
+    }
+
+    routing {
+        get("/") {
+            call.respondText("Ktor backend is running!")
+        }
+
+        get("/notes") {
+            val notes = notesCollection.find().toList()
+            call.respond(notes)
+        }
+    }
+}
+
+fun main() {
+    embeddedServer(Netty, port = 8080, module = Application::module).start(wait = true)
+}
